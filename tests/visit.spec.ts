1b76cd9d9ef692576c9595decedf9d27dc0b640b
sosukesuzuki <aosukeke@gmail.com>
2022-04-25 20:46:53 +0900

    Add tests for `visit`

--- tests/visit.spec.ts
+++ tests/visit.spec.ts
@@ -1,5 +1,21 @@
 import test from "node:test";
+import { visit } from "../src/visit";
+import { parse } from "../src/parser";
+import assert from "node:assert/strict";
 
 test("visit", () => {
-  // noop
+  const expected = ["Program", "Assign", "Text", "Condition", "Text"];
+  const template = `<#assign f=1>
+  <#if f gt 0>
+    \${f} > 0
+  <#else>
+    \${f} < 0
+  </#if>
+  `;
+  visit(parse(template), (node) => {
+    if (node.type === expected[0]) {
+      expected.shift();
+    }
+  });
+  assert(expected.length === 0);
 });
