a86f73c3a2b0ee66845b1123a3dfe7f1393f2128
sosukesuzuki <aosukeke@gmail.com>
2022-04-26 18:02:14 +0900

    Fix index.ts

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ftl-dependency-analyzer
+
+Analyze FreeMarker Template Language (FTL).
+
+## CLI
+
+```
+$ ftl-dependency-analyze ./src/index.ftl
+|--includes-01.ftl
+  |--/includes-02.ftl
+    |--/basic-01.ftl
+    |--/basic-02.ftl
+  |--/basic-03.ftl
+
+```
+### Options
+
+#### `--format`, `-f`
+
+#### `--base`, `-b`
+
+## API
+
+### `parse`
+
+### `visit`
+
+### `analyzeIncludeDirective`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import path from "node:path";
-import { analyzeIncludeDirective } from "./analyze";
-import { viewTree } from "./viewers";
-
-const fileName = "includes-01.ftl";
-const baseDir = path.join(process.cwd(), "fixtures");
-const res = analyzeIncludeDirective(fileName, baseDir);
-
-console.log(viewTree(res));
+export { parse } from "./parser";
+export { visit } from "./visit";
+export { analyzeIncludeDirective } from "./analyze";
